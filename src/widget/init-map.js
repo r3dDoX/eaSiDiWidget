8acf4c12fd23c7fa9f9ca706b59adf7e0b330561
r3dDoX <patrick@walther-online.ch>
2019-08-15 20:27:25 +0200

    add drawing tools and increase size of iframe

--- src/widget/init-map.js
+++ src/widget/init-map.js
@@ -5,14 +5,19 @@ export function initArcGis(dojoRequire) {
     'esri/Map',
     'esri/views/MapView',
     'esri/layers/FeatureLayer',
-  ], function (Map, MapView, FeatureLayer) {
+    'esri/layers/GraphicsLayer',
+    'esri/widgets/Sketch',
+  ], function (Map, MapView, FeatureLayer, GraphicsLayer, Sketch) {
+
+    const graphicsLayer = new GraphicsLayer();
 
     const map = new Map({
       basemap: 'topo-vector',
+      layers: [graphicsLayer],
     });
 
     const trailsLayer = new FeatureLayer({
-      url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0"
+      url: 'https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0',
     });
 
     map.add(trailsLayer, 0);
@@ -25,11 +30,18 @@ export function initArcGis(dojoRequire) {
 
     map.add(trailHeads);
 
-    new MapView({
+    const view = new MapView({
       container: 'viewDiv',
       map: map,
-      center: [-118.80543,34.02700], // longitude, latitude
+      center: [-118.80543, 34.02700], // longitude, latitude
       zoom: 13,
     });
+
+    const sketch = new Sketch({
+      view: view,
+      layer: graphicsLayer,
+    });
+
+    view.ui.add(sketch, 'top-right');
   });
 }
